ac39167448b019ad09df3e44e2a09f207f073b21
sarmad <sarmad-chandio@uiowa.edu>
2021-09-14 22:13:55 -0500

    Update Rectangle.java

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,17 +1,22 @@
 /*
-Create a class called Rectangle that uses the Polygon interface
+Create a class called Rectangle that implements the Polygon interface
 Must have a default constructor that sets:
     double: width = 1
     double: height = 1
 
 Must have another Constructor that takes in:
-    double: width
-    double: height
+    double: width --> 1st argument
+    double: height --> 2nd argument
 
-Must have setters and getters
+Must have setters and getters for width and height.
+double getWidth()
+double getHeight()
+void setHeight()
+void setWidth()
+
 
 Must have a toString (test Provided)
-    toString is in the format:
+    "String toString()" should return the following:
         "Rectangle width & height: " + width + ", " + height
  */
 
