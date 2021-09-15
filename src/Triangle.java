a29eb3a9b7a3b201538671eda361ecd8f6b1b747
sarmad <sarmad-chandio@uiowa.edu>
2021-09-14 22:17:23 -0500

    Update Triangle.java

--- src/Triangle.java
+++ src/Triangle.java
@@ -4,21 +4,25 @@ import java.lang.Math;
 Create a class called Triangle that uses the Polygon interface;
     Assume this is a right triangle
 
+
 Must have a default constructor that sets:
     a = 3
     b = 4
     c = 5
 
 Must have a Constructor that takes in 3 variables:
-    a
-    b
-    c
+    a --> 1st argument
+    b --> 2nd argument
+    c --> 3rd argument
 
-Must have setters and getters
-    get
+Must have setters and getters for all the variables
+    double getA()
+    void setA()
+    ...
+    ...
 
 Must have a toString (test Provided)
-    toString is in the format:
+    "String toString()" Should return the following:
         "Triangle edge lengths: " + a + ", " + b + ", " + c
  */
 
