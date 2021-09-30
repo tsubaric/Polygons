7b513e5a3992edbf900cf020310b3c3f0da48123
Tommy Subarc <tsubaric@uiowa.edu>
2021-09-29 19:35:21 -0500

    final

--- src/Triangle.java
+++ src/Triangle.java
@@ -37,30 +37,3 @@ public class Triangle implements Polygon{
         return ("Triangle edge lengths: " + a + ", " + b + ", " + c);
     }
 }
-/*
-Create a class called Triangle that uses the Polygon interface;
-    Assume this is a right triangle
-
-    double  a, b, c;
-
-Must have a default constructor that sets:
-    a = 3
-    b = 4
-    c = 5
-
-Must have a Constructor that takes in 3 variables:
-    a --> 1st argument
-    b --> 2nd argument
-    c --> 3rd argument
-
-Must have setters and getters for all the variables
-    double getA()
-    void setA()
-    ...
-    ...
-
-Must have a toString (test Provided)
-    "String toString()" Should return the following:
-        "Triangle edge lengths: " + a + ", " + b + ", " + c
- */
-
